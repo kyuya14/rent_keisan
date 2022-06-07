5a356eaf628a33a9d60b803eb30e25390fea024e
Ubuntu <ubuntu@ip-10-40-10-236.ap-northeast-1.compute.internal>
2022-06-07 12:16:42 +0000

    パラメータ微修正

--- owned_house.py
+++ owned_house.py
@@ -3,22 +3,19 @@
 
 
 # your parameter
-# parsonal 
-from sre_constants import RANGE
-
-
+# 変数値（入力値）
 LIMIT = 35                             # 期間
 START_YEAR = 2025                      # 年
 START_AGE = 35                         # 年齢
-END_YEAR = START_YEAR + LIMIT          # 年
-END_AGE = START_AGE + LIMIT            # 年齢
-
-# basic parameter
 HOUSE_COST = 5000 * 10000              # 住宅費用
 ATAMAKIN = 0                           # 頭金
-FIRST_COST = HOUSE_COST * 0.06         # 初期費用（住宅費用×0.06）
 UTILITY_COST = 20000                   # 光熱費（月額）
 RATE = 0.015
+
+# 固定値
+END_YEAR = START_YEAR + LIMIT          # 年
+END_AGE = START_AGE + LIMIT            # 年齢
+FIRST_COST = HOUSE_COST * 0.06         # 初期費用（住宅費用×0.06）
 KOTEISHISAN = 150000                   # 固定資産税（年額）
 SHUZENHI = 5500000                     # 修繕費用
 
@@ -32,20 +29,20 @@ RANGE = range(0, LIMIT * 12)
 # 金利計算（元利均等返済）※後で関数にする
 
 totalcost =  HOUSE_COST - ATAMAKIN
-balance = totalcost
+loan_balance = totalcost  # ローン残高
 r = RATE
 for month in RANGE:
-    if balance < 0: break
+    if loan_balance < 0: break
     # 毎月返済額(reduction_month)の計算
     reduction_month = (totalcost * r / 12 * (1 + r / 12)**(LIMIT * 12)) / (((1 + r / 12)**(LIMIT * 12)) - 1)
-    # 金利 
-    kinri = balance * r / 12
+    # 金利(kinri)
+    kinri = loan_balance * r / 12
     # 元金分
     gankin = reduction_month - kinri
-    # 残高(balance)
-    balance = balance - gankin
+    # ローン残高(loan_balance)
+    loan_balance = loan_balance - gankin
 
-    print(int(reduction_month),int(balance),int(reduction_month-kinri),int(kinri))
+    print(int(reduction_month),int(loan_balance),int(reduction_month-kinri),int(kinri))
 
 # --------------------------------------
 
